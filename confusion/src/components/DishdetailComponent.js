c1fa7dd1474af4180feb83f593aa47517db4d95e
Zernez <fernando.pannullo@yahoo.it>
2021-08-22 11:13:07 +0200

    Single Page Applications Part 2

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
     
-        function RenderDish({dish}) {
+    function RenderDish({dish}) {
         if (dish != null) {
             
             return(
@@ -20,46 +20,70 @@ import { Card, CardImg, CardText, CardBody,
             else {
                 return(
                     <div></div>
-                )
-            }
-            
+                );
+            }           
         }
     
-        function RenderComments({comments}) {
+    function RenderComments({comments}) {
         if (comments != null) {
             
             return (
-                <div>
+                <div className= "col-12 col-md-4 m-2">
                     <h4>Comments</h4>
-                    {comments.map(function(name, index){
+                    <ul className= "list-unstyled">
+                    {comments.map(function(comment){
                     return (
-                        <div className="list-unstyled">
-                        <p>{comments[index].comment}</p>
-                        <p>--{comments[index].author}, 
+                        <li key= {comment.id}>
+                        <p>{comment.comment}</p>
+                        <p>--{comment.author}, 
                         {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))}
                         </p>
-                        </div>
+                        </li>
                         );
                     })}
+                    </ul>
                 </div>
-                    )                
-            }
-            else {
-                return(
-                    <div></div>
-                )
-            }
+                    );                
         }
+        else {
+            return(
+                <div></div>
+            );
+        }
+    }
     
-        const DishDetail = (props) => {
-        return ( 
-            <div className='container'>
-                <div className='row'>
-                    {this.renderDish(props.selectedDish)}
-                    {this.renderComments(props.selectedDish)}
+    const DishDetail = (props) => {
+        if (props.dish != null) {
+            
+            return (
+                <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
                 </div>
-            </div>
-        )
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
+                    </div>
+                </div>
+                </div>
+            );
+        }
+        else {
+            return(
+            <div></div>
+            );
+        }
     }
 
     export default DishDetail;
